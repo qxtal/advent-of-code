c8e0d6814cea563da9a0c6f9288d9e8e18913000
Quartz Xtal <quartz@xtal.sh>
2022-12-01 15:37:42 +0100

    AoC 2022 Day 1

---
+++ 2022/01/aoc-01b.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Advent of Code 2022: Day 1 (part 2)
+# Submission by qxtal <quartz@xtal.sh>
+
+f = open('./input.txt', 'r') # hard-coded input file ¯\_(ツ)_/¯
+elf_input = f.readlines()
+f.close()
+
+elf_calories = [0] # first element initialized with zero
+
+for line in elf_input:
+    if line.strip().isdigit():
+        elf_calories[-1] += int(line)
+    else:
+        elf_calories.append(0) # add new element, init with zero
+
+elf_calories.sort(reverse=True)
+top_three_sum = sum(elf_calories[:3])
+
+print(top_three_sum)
